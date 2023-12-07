49bdce47f550fab35f99580114ff556c5c37e25c
jackbangindc <jackbangindc@gmail.com>
2023-12-08 00:38:29 +0800

    2023-12-08 00:38:29

--- main.py
+++ main.py
@@ -72,3 +72,4 @@ def detect_language(
     language_probs = [
         {
             c: language_token_probs[i, j].item()
+            for j, c in zip(tokenizer.all_language_tokens, tokenizer.all_language_codes)
