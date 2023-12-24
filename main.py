59f284f43cc9b10d69457a018240e85c5e93fa7b
jackbangindc <jackbangindc@gmail.com>
2023-12-25 01:27:04 +0800

    2023-12-25 01:27:04

--- main.py
+++ main.py
@@ -98,3 +98,4 @@ class DecodingOptions:
     best_of: Optional[int] = None  # number of independent sample trajectories, if t > 0
     beam_size: Optional[int] = None  # number of beams in beam search, if t == 0
     patience: Optional[float] = None  # patience in beam search (arxiv:2204.05424)
+
