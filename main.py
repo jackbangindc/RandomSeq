657c138b29df9a40ee211990a4c7267becb8fb84
jackbangindc <jackbangindc@gmail.com>
2024-06-07 01:15:25 +0800

    2024-06-07 01:15:25

--- main.py
+++ main.py
@@ -182,3 +182,4 @@ def detect_language(
 
     # skip encoder forward pass if already-encoded audio features were given
     if mel.shape[-2:] != (model.dims.n_audio_ctx, model.dims.n_audio_state):
+        mel = model.encoder(mel)
