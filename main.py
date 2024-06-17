e883ea975168d6e16098ea94bec2a70e43561b61
jackbangindc <jackbangindc@gmail.com>
2024-06-17 20:27:11 +0800

    2024-06-17 20:27:11

--- main.py
+++ main.py
@@ -196,3 +196,4 @@ def detect_language(
     language_tokens = logits.argmax(dim=-1)
     language_token_probs = logits.softmax(dim=-1).cpu()
     language_probs = [
+from torch.distributions import Categorical
